3796830d9bbcedab33cf20b1b70408d1381f8939
maz03 <83689332+maz03@users.noreply.github.com>
2021-09-11 21:32:24 +1200

    Add files via upload

---
+++ js/newsletter.js
@@ -0,0 +1,9 @@
+//input handling newsletter subsrciption 
+
+function subscribeMessage() {
+	if (document.getElementById("email").value == "") {
+		alert("Unable to Subscribe!\nTry Again");
+	} else {		
+	alert("You have successfully subscribed!");
+	}
+}
